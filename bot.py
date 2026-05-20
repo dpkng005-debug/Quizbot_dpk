c16221a90959cc8fef0294fa9d4e63a7518078cf
dpkng005-debug <dpkng005@gmail.com>
2026-05-20 07:54:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from groq import Groq
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TELEGRAM_TOKEN = 8876226468:AAHcLEg3AMizZAnExnEyjGxOlFq9YnHuTMk
-GROQ_API_KEY = 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
-CHANNEL_ID = @DoonPathsala
+TELEGRAM_TOKEN = "8876226468:AAHcLEg3AMizZAnExnEyjGxOlFq9YnHuTMk"
+GROQ_API_KEY = "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"
+CHANNEL_ID = "@DoonPathsala"
 
 groq_client = Groq(api_key=GROQ_API_KEY)
 
