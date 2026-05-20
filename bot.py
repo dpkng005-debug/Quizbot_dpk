a559ec34fe80a1dfa39e5fb872fa8dca9d584c98
dpkng005-debug <dpkng005@gmail.com>
2026-05-20 08:22:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ Text:
 {text}
 """
     response = groq_client.chat.completions.create(
-        model="llama3-8b-8192",
+       model="llama-3.3-70b-versatile",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.1,
         max_tokens=3000,
