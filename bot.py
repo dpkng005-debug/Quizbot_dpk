defcbc3b5b28634c6b06b93aea5d57f190e0c16f
dpkng005-debug <dpkng005@gmail.com>
2026-05-20 17:32:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,9 +20,9 @@ PORT = int(os.environ.get("PORT", 8080))  # Render ke liye dynamic port fixation
 
 # API Configuration
 # client_options ka use karke v1 API ko force karna
-genai.configure(api_key=GEMINI_API_KEY)
+# Library ko direct parameter se bol rahe hain ki beta version mat use karo
+genai.configure(api_key=GEMINI_API_KEY, client_options={"api_version": "v1"})
 model = genai.GenerativeModel("gemini-1.5-flash")
-
 # Dummy HTTP Server Render ki active checking ke liye
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
